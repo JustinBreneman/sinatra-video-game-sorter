f0952353d4797e364e4d9c3a1d8b792d615c0f6b
JustinBreneman <Justin.Breneman@yahoo.com>
2021-01-16 19:27:10 -0800

    added user profile edit functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,25 @@ class UsersController < ApplicationController
             redirect to '/login'
         end
     end
+
+    get '/users/:slug/edit' do
+        @user = User.find_by_slug(params[:slug])
+        if @user && Helpers.is_logged_in?(session)
+            @platforms = Platform.all
+            erb :'users/edit'
+        else
+            redirect to '/login'
+        end
+    end
+
+    patch '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        game_ids = params[:games]
+        games = []
+        game_ids.each do |id|
+            games << Game.find(id)
+        end
+        @user.update(games: games)
+        redirect to "/users/#{@user.slug}"
+    end
 end
