27187d815aab49392fdd56d1af5ab3cf3011bf4e
JustinBreneman <Justin.Breneman@yahoo.com>
2021-01-16 16:31:51 -0800

    removed unneccessary helper method

--- app/lib/helpers.rb
+++ app/lib/helpers.rb
@@ -3,12 +3,6 @@ class Helpers
         !!session[:id]
     end
 
-    def self.current_user(session)
-        if self.is_logged_in?(session)
-            User.find(session[:id])
-        end
-    end
-
     def self.slugify(phrase)
         if !!phrase.gsub!(/[^0-9A-Za-z ]/, '')
             phrase.split(" ").join("-")
