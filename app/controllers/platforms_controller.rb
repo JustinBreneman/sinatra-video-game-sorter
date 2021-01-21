af32a90809e323cffb22630226d68ebb67476694
JustinBreneman <Justin.Breneman@yahoo.com>
2021-01-20 22:10:39 -0800

    added create platforms functionality

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -4,4 +4,17 @@ class PlatformsController < ApplicationController
         @platforms = Platform.all
         erb :'platforms/index'
     end
+
+    get '/platforms/new' do
+        if Helpers.is_logged_in?(session)
+            erb :'platforms/new'
+        else
+            redirect to '/login'
+        end
+    end
+
+    post '/platforms/new' do
+        platform = Platform.create(name: params[:name], manufacturer: params[:manufacturer])
+        redcirect to '/platforms/'
+    end
 end
