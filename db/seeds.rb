808c2b2b5c078d9c16b9b6790a9a5a4aff60d5c2
JustinBreneman <Justin.Breneman@yahoo.com>
2021-01-27 16:17:24 -0800

    moved the user creation seeds down in the file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
-user1 = User.create(username: "user1", email: "user1@email.com", password: "user1")
-user2 = User.create(username: "user2", email: "user2@email.com", password: "user2")
-user3 = User.create(username: "user3", email: "user3@email.com", password: "user3")
-user4 = User.create(username: "user4", email: "user4@email.com", password: "user4")
-user5 = User.create(username: "user5", email: "user5@email.com", password: "user5")
-user6 = User.create(username: "user6", email: "user6@email.com", password: "user6")
-user7 = User.create(username: "user7", email: "user7@email.com", password: "user7")
-user8 = User.create(username: "user8", email: "user8@email.com", password: "user8")
-user9 = User.create(username: "user9", email: "user9@email.com", password: "user9")
-user10 = User.create(username: "user10", email: "user10@email.com", password: "user10")
-
 ps4 = Platform.create(name: "PS4", manufacturer: "Sony")
 ps3 = Platform.create(name: "PS3", manufacturer: "Sony")
 xbox360 = Platform.create(name: "X-Box 360", manufacturer: "Microsoft")
@@ -29,6 +18,17 @@ game9 = xboxone.games.build(title: "Warframe", developer: "Digital Extremes", re
 game10 = switch.games.build(title: "Warframe", developer: "Digital Extremes", release_date: "20 November 2018")
 game11 = switch.games.build(title: "Skyrim Special Edition", developer: "Bethesda", release_date: "17 November 2017")
 game12 = alexa.games.build(title: "Skyrim Very Special Edition", developer: "Bethesda", release_date: "10 June 2018")
+
+user1 = User.create(username: "user1", email: "user1@email.com", password: "user1")
+user2 = User.create(username: "user2", email: "user2@email.com", password: "user2")
+user3 = User.create(username: "user3", email: "user3@email.com", password: "user3")
+user4 = User.create(username: "user4", email: "user4@email.com", password: "user4")
+user5 = User.create(username: "user5", email: "user5@email.com", password: "user5")
+user6 = User.create(username: "user6", email: "user6@email.com", password: "user6")
+user7 = User.create(username: "user7", email: "user7@email.com", password: "user7")
+user8 = User.create(username: "user8", email: "user8@email.com", password: "user8")
+user9 = User.create(username: "user9", email: "user9@email.com", password: "user9")
+user10 = User.create(username: "user10", email: "user10@email.com", password: "user10")
 
 user1.games = [game1, game3, game7, game9, game11, game12]
 user2.games = [game2, game4, game6, game8, game10, game11]
