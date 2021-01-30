bac00920fbd73119821db33147345d765e773484
JustinBreneman <Justin.Breneman@yahoo.com>
2021-01-29 16:26:48 -0800

    added edit path and veiw to platforms

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -23,4 +23,28 @@ class PlatformsController < ApplicationController
         @platform = Platform.find_by_slug(params[:slug])
         erb :'/platforms/show'
     end
+
+    get '/platforms/:slug/edit' do
+        @platform = Platform.find_by_slug(params[:slug])
+        if Helpers.is_logged_in?(session)
+            erb :'platforms/edit'
+        else
+            flash[:message] = "Please login to edit a Platform."
+            redirect to "/platforms/#{params[:slug]}"
+        end
+    end
+
+    post '/platforms/:slug' do
+        platform = Platform.find_by_slug(params[:slug])
+        if !params[:name] || params[:name] == ''
+            flash[:message] = "Platforms must have a name."
+            redirect to "/platforms/#{params[:slug]}/edit"
+        elsif !params[:manufacturer] || params[:manufacturer] == ''
+            flash[:message] == "Platforms must have a manufacturer."
+            redirect to "/platforms/#{params[:slug]}/edit"
+        else
+            platform.update(name: params[:name], manufacturer: params[:manufacturer])
+            redirect to "/platforms/#{platform.slug}"
+        end
+    end
 end
